e6f4204123f71d91b4587a4ef08b9f8937c5c672
Ishan <ishan.kulkarni@somaiya.edu>
2022-02-02 12:02:15 +0530

    FEATURE: DB connection done

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,22 @@
 console.log("Hello")
+
+import { createConnection, Connection } from "typeorm"
+
+const connect = async () => {
+    try {
+        const connection: Connection = await createConnection({
+            type: "mysql",
+            host: "localhost",
+            port: 3306,
+            username: "admin",
+            password: "password",
+            database: "typeorm"
+        })
+    }
+    catch (error) {
+        console.error(error)
+        throw new Error("Unable to connect to the db")
+    }
+}
+
+connect();
