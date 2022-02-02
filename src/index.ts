5c5d4ea1951a22bd183643c32de8788d179fb240
Ishan <ishan.kulkarni@somaiya.edu>
2022-02-03 03:09:01 +0530

    FEATURE: Entities added

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,26 @@
 console.log("Hello")
 
 import { createConnection, Connection } from "typeorm"
+import { Client } from "./entities/client";
 
+var connection: Connection
 const connect = async () => {
     try {
-        const connection: Connection = await createConnection({
+        connection = await createConnection({
             type: "mysql",
             host: "localhost",
             port: 3306,
             username: "admin",
             password: "password",
-            database: "typeorm"
+            database: "typeorm",
+            "synchronize": true,
+            entities: [Client]
         })
+        console.log(connection)
     }
     catch (error) {
+        console.log(connection)
+
         console.error(error)
         throw new Error("Unable to connect to the db")
     }
